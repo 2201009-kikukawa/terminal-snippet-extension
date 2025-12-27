8004f5fd012cd249c481423e1c25a49b79f079d7
kikukawa <2201009@s.asojuku.ac.jp>
2025-12-27 17:16:41 +0900

    chadcn/uiへのリプレイス

--- src/webview/components/common/Option.tsx
+++ src/webview/components/common/Option.tsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { VSCodeOption } from "@vscode/webview-ui-toolkit/react";
+import Button from "./Button";
+import { cn } from "../../lib/utils";
 
-interface OptionProps {
-  onClick: () => void;
-  children: React.ReactNode;
-  className?: string;
-}
+interface OptionProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Option: React.FC<OptionProps> = ({ children, ...rest }) => {
-  return <VSCodeOption {...rest}>{children}</VSCodeOption>;
+const Option: React.FC<OptionProps> = ({ children, className, ...rest }) => {
+  return (
+    <Button
+      appearance="ghost"
+      className={cn(
+        "w-full justify-start rounded-sm px-2 py-1 text-left text-sm hover:bg-[var(--accent)]/10",
+        className
+      )}
+      {...rest}>
+      {children}
+    </Button>
+  );
 };
 
 export default Option;
